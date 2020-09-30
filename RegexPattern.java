371a6a0f55ad964b4ba36d98c3d2412b60c745b6
sharale34 <sourabhharale8000@gmail.com>
2020-09-30 10:32:23 +0530

    Added: Restriction Added From Taking Alphabets/Special Characters at Beginning

--- RegexPattern.java
+++ RegexPattern.java
@@ -1,9 +1,23 @@
 package com.BridgeLabz.Regex;
-
+import java.util.Scanner;
+import java.util.regex.*;
 public class RegexPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Welcome to PIN Code Validation Program");
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter the PIN Code :");
+    String pin = sc.nextLine();
+    String pattern ="[1-9][0-9]{5}";
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(pin);
+    if(m.matches())
+    {
+    	System.out.println("PIN Code is validated");
+    }
+    else
+    {
+    	System.out.println("PIN Code is not validated");
+    }
 	}
 }
